f41223a9080fa1ddeb4be5a098788b43c95e6ae1
Benjamin Frey <124636707+benjaminfreyuu@users.noreply.github.com>
2025-01-09 18:43:19 +0100

    Update visualize_attention_Celldeath.py

--- visualization/visualize_attention_Celldeath.py
+++ visualization/visualize_attention_Celldeath.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Changes made: Added adjusted data loader and fold_channel functions to work with custom input. 
 import os
 import sys
 import argparse
